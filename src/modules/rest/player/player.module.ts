d6c73a6e7cee189b49b914c04c093131548b3084
officefish <indicogames@yandex.ru>
2024-09-08 15:31:54 +0300

    TokenModule added to PlayerModule

--- src/modules/rest/player/player.module.ts
+++ src/modules/rest/player/player.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common'
 import { GameplayModule } from '@/modules/gameplay/gameplay.module'
 import { GameplayService } from '@/modules/gameplay/gameplay.service'
 import { PlayerController } from './player.controller'
+import { TokenModule } from '@/modules/token/token.module'
 
 @Module({
   imports: [
-   GameplayModule
+   GameplayModule,
+   TokenModule,
   ],
   controllers : [PlayerController],
   providers: [GameplayService],
